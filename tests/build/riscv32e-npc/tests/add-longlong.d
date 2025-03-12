4dcaa6c52c661f2d39a759801f8912b5b4c6352e
YCX <CrystalCracks@outlook.com>
2025-03-12 21:00:05 +0800

    modify makefile

--- tests/build/riscv32e-npc/tests/add-longlong.d
+++ tests/build/riscv32e-npc/tests/add-longlong.d
@@ -1,8 +1,8 @@
-/home/crystc/ysyx-workbench/am-kernels/tests/cpu-tests/build/riscv32e-npc/tests/add-longlong.o: \
- /home/crystc/ysyx-workbench/am-kernels/tests/cpu-tests/tests/add-longlong.c \
- /home/crystc/ysyx-workbench/am-kernels/tests/cpu-tests/include/trap.h \
- /home/crystc/ysyx-workbench/abstract-machine/am/include/am.h \
- /home/crystc/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
- /home/crystc/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/crystc/ysyx-workbench/abstract-machine/klib/include/klib.h \
- /home/crystc/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
+/home/crystc/workspace/RV32-CPU/tests/build/riscv32e-npc/tests/add-longlong.o: \
+ /home/crystc/workspace/RV32-CPU/tests/tests/add-longlong.c \
+ /home/crystc/workspace/RV32-CPU/tests/include/trap.h \
+ /home/crystc/workspace/RV32-CPU/abstract-machine/am/include/am.h \
+ /home/crystc/workspace/RV32-CPU/abstract-machine/am/include/arch/riscv.h \
+ /home/crystc/workspace/RV32-CPU/abstract-machine/am/include/amdev.h \
+ /home/crystc/workspace/RV32-CPU/abstract-machine/klib/include/klib.h \
+ /home/crystc/workspace/RV32-CPU/abstract-machine/klib/include/klib-macros.h
